cedca53ceccf68545d57c66969c74b35cad6fbf0
cmd-kvn <kvn.m.wong@gmail.com>
2017-02-20 13:57:06 -0800

    fixed eslint indent errors

--- test/error-handler-test.js
+++ test/error-handler-test.js
@@ -2,14 +2,14 @@ const errorHandler = require('../lib/error-handler')();
 const assert = require('chai').assert;
 
 describe.only('test error handler', () => {
-    it('returns 500 when no code is provided', () => {
-        const err = new Error('some server error');
-        const res = {
-            status(code) { this.code = code; return this; },
-            send(error) { this.error = error; }
-        };
-        errorHandler(err, null, res, null);
-        assert.equal(res.code, 500);
-        assert.deepEqual(res.error.error, 'internal server error');
-    });
+	it('returns 500 when no code is provided', () => {
+	    const err = new Error('some server error');
+	    const res = {
+	    status(code) { this.code = code; return this; },
+	    send(error) { this.error = error; }
+	};
+	    errorHandler(err, null, res, null);
+	    assert.equal(res.code, 500);
+	    assert.deepEqual(res.error.error, 'internal server error');
+	});
 });
